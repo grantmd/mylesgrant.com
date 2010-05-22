dde0101fa97fe3c70b84f4f243274d440679e524
Myles Grant <myles@mylesgrant.com>
2010-05-22 09:46:17 -0700

    Adding app stats and a language header

---
+++ import.sh
@@ -0,0 +1,2 @@
+appcfg.py upload_data --config_file=loaders.py --filename=posts.csv --kind=Post --url=http://localhost:8083/your_moms_api .
+appcfg.py upload_data --config_file=loaders.py --filename=comments.csv --kind=Comment --url=http://localhost:8083/your_moms_api .
