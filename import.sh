a042258075318a5e1db8b20628bdc900d57a6b7c
Myles Grant <myles@mylesgrant.com>
2010-05-22 09:49:38 -0700

    More stuff I didn't want to check in

--- import.sh
+++
@@ -1,2 +0,0 @@
-appcfg.py upload_data --config_file=loaders.py --filename=posts.csv --kind=Post --url=http://localhost:8083/your_moms_api .
-appcfg.py upload_data --config_file=loaders.py --filename=comments.csv --kind=Comment --url=http://localhost:8083/your_moms_api .
